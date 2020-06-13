6d0db998f2b9b17c2df4e17504e0eb5171ea01b0
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-06-13 16:42:43 -0700

    Cleaned up grammar

--- app.js
+++ app.js
@@ -2,48 +2,57 @@
 const store = {
   questions: [
     {
-      question: 'Hopoo Games is an indie studio, comprised of how many developers?',
-      answers: [
-        'Three',
-        'Ten',
-        'Seventeen',
-        'Forty'
-      ],
+      question:
+        'Hopoo Games is an indie studio, comprised of how many developers?',
+      answers: ['Three', 'Ten', 'Seventeen', 'Forty'],
       correctAnswer: 0,
       incorrectDesc: 'Hopoo currently employs three devs.',
-      factoid: 'Hopoo Games has three employees, four if you count the composer writing the soundtrack.'
-    },
-    {
-      question: 'There are items in both installments that are references the devs. What is one such item?',
-      answers: ['Halcyon Seed', '56 Leaf Clover', 'Hopoo Feather', 'Happiest Mask'],
-      correctAnswer: 2,
-      incorrectDesc: 'Hopoo Feather, Paul\'s Goat Hoof, and Ghor\'s Tome all reference a developer.',
       factoid:
-        'Ghor\'s Tome did not exist in the original game, as Hopoo Games was comprised of only Paul and Hopoo until 2018.',
+        'Hopoo Games has three employees, four if you count the composer writing the soundtrack.',
     },
     {
-      question: 'Which environment in Risk of Rain 2 is the only returning stage from the first game?',
+      question:
+        'There are items in both installments that are references to the devs. What is one such item?',
+      answers: [
+        'Halcyon Seed',
+        '56 Leaf Clover',
+        'Hopoo Feather',
+        'Happiest Mask',
+      ],
+      correctAnswer: 2,
+      incorrectDesc:
+        "Hopoo Feather, Paul's Goat Hoof, and Ghor's Tome all reference a developer.",
+      factoid:
+        "Ghor's Tome did not exist in the original game, as Hopoo Games was comprised of only Paul and Hopoo until 2018.",
+    },
+    {
+      question:
+        'Which environment in Risk of Rain 2 is the only returning stage from the first game?',
       answers: [
         'Abandoned Aqueduct',
         'Sky Meadow',
         'Distant Roost',
-        'Desolate Forest'
+        'Desolate Forest',
       ],
       correctAnswer: 1,
-      incorrectDesc: 'Sky Meadow is the only stage that has been reimagined for the second installment.',
-      factoid: 'The soundtrack for this stage is also the only soundtrack anywhere in the game that uses interpolations from the first game\'s soundtrack.'
+      incorrectDesc:
+        'Sky Meadow is the only stage that has been reimagined for the second installment.',
+      factoid:
+        "The soundtrack for this stage is also the only soundtrack anywhere in the game that uses interpolations from the first game's soundtrack.",
     },
     {
-      question: 'What item can you only acquire after having unlocked the achievement Never Back Down?',
+      question:
+        'What item can you only acquire after having unlocked the achievement Never Back Down?',
       answers: [
         'Strides of Heresy',
         'Transcendence',
         'Focused Convergence',
-        'Shaped Glass'
+        'Shaped Glass',
       ],
       correctAnswer: 2,
       incorrectDesc: 'Never Back Down unlocks Focused Convergence for use.',
-      factoid: 'Focused Convergence is a Lunar item that increases teleporter charge rate by 30%, but reduces teleporter charge radius by 50% for each stack of the item.'
+      factoid:
+        'Focused Convergence is a Lunar item that increases teleporter charge rate by 30%, but reduces teleporter charge radius by 50% for each stack of the item.',
     },
     {
       question: 'Who composed the soundtrack for Risk of Rain 2?',
@@ -51,23 +60,25 @@ const store = {
         'Chris Christodoulou',
         'Daniel Doulou',
         'Chris Doulou',
-        'Daniel Christodoulou'
+        'Daniel Christodoulou',
       ],
       correctAnswer: 0,
-      incorrectDesc: 'Chris Christodoulou composed the soundtrack for Risk of Rain 2',
-      factoid: 'Chris has also composed the soundtrack for all of Hopoo\'s games, including Deadbolt and the original Risk of Rain.'
-    }
+      incorrectDesc:
+        'Chris Christodoulou composed the soundtrack for Risk of Rain 2',
+      factoid:
+        "Chris has also composed the soundtrack for all of Hopoo's games, including Deadbolt and the original Risk of Rain.",
+    },
   ],
   quizStarted: false,
   questionNumber: 0,
   score: 0,
-  answer: ''
+  answer: '',
 };
 
 /*
- * 
+ *
  * Technical requirements:
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * Your app should include a render() function, that regenerates the view each time the store is updated.
  * [See your course material, consult your instructor, and reference the slides for more details.]
  * NO additional HTML elements should be added to the index.html file.
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary.
@@ -80,72 +91,71 @@ const store = {
 function generateQuizQuestion() {
   let html = '';
   html = `<section class='js-main-screen'>
-            <h2 class='js-question-box'>${store.questions[store.questionNumber].question}</h2>
-            <form action='' id='js-answer-form' class='js-answer-box'>`;
+            <h2 class='js-question-box'>${
+              store.questions[store.questionNumber].question
+            }</h2>
+            <form action="#" id='js-answer-form' class='js-answer-box'>`;
 
   let i = 1;
-  store.questions[store.questionNumber].answers.forEach(quizAnswer => {
-
+  store.questions[store.questionNumber].answers.forEach((quizAnswer) => {
     html += `<div class='input-container'>
-    <input type="radio" required name="answer" id='answer${i}' value='${quizAnswer}'>
-    <label for='answer${i}'>${quizAnswer}</label>
+    <input type="radio" required name="answer" id="answer${i}" value="${quizAnswer}">
+    <label for="answer${i}">${quizAnswer}</label>
     </div>`;
     i++;
   });
 
-  html += `<section class='js-answer-eval'></section>
-         <button type ='submit' class='js-answer-button'>Submit</button>`;
+  html += `<section class="js-answer-eval"></section>
+         <button type ="submit" class="js-answer-button">Submit</button>`;
   html += generateQuizCount();
   html += `</form>
          </section>`;
   return html;
 }
 
-
 function generateQuizFeedback() {
   let html = '';
   let incorrectFlag = false;
   let currentQuestion = store.questions[store.questionNumber];
   let correct = currentQuestion.answers[currentQuestion.correctAnswer];
-  html += `<section class='js-main-screen'>
-              <h2 class='js-question-box'>${currentQuestion.question}</h2>
-              <form action='' id='js-answer-form' class='js-answer-box'>`;
+  html += `<section class="js-main-screen">
+              <h2 class="js-question-box">${currentQuestion.question}</h2>
+              <form action="#" id="js-answer-form" class="js-answer-box">`;
 
   let i = 1;
-  currentQuestion.answers.forEach(quizAnswer => {
-
+  currentQuestion.answers.forEach((quizAnswer) => {
     if (quizAnswer !== correct && quizAnswer === store.answer) {
-
       incorrectFlag = !incorrectFlag;
-      html += '<div class=\'input-container incorrect\'>';
+      html += '<div class="input-container incorrect">';
     } else {
       if (quizAnswer === correct) {
-
-        html += '<div class=\'input-container correct\'>';
+        html += '<div class="input-container correct">';
         if (correct === store.answer) {
           store.score++;
         }
       } else {
-        html += '<div class = \'input-container\'>';
+        html += '<div class = "input-container">';
       }
     }
-    html += `<input disabled ${store.answer === quizAnswer ? 'checked' : ''} type="radio" name="answer" id='answer${i} value='${quizAnswer}'>`;
-    html += `<label for='answer${i}'>${quizAnswer}</label>`;
+    html += `<input disabled ${
+      store.answer === quizAnswer ? 'checked' : ''
+    } type="radio" name="answer" id="answer${i}" value="${quizAnswer}">`;
+    html += `<label for="answer${i}">${quizAnswer}</label>`;
     html += '</div>';
     i++;
   });
 
   if (incorrectFlag) {
-    html += `<section class='js-answer-eval incorrect'><strong>
+    html += `<section class="js-answer-eval incorrect"><strong>
     Incorrect!</strong> ${currentQuestion.incorrectDesc} ${currentQuestion.factoid}
     </section>`;
   } else {
-    html += `<section class='js-answer-eval correct'><strong>
+    html += `<section class="js-answer-eval correct"><strong>
     Correct!</strong> ${currentQuestion.factoid}
     </section>`;
   }
 
-  html += '<button type =\'submit\' class=\'js-continue-button\'>Continue</button>';
+  html += '<button type ="submit" class="js-continue-button">Continue</button>';
   html += generateQuizCount();
   html += `</form>
           </section>`;
@@ -154,28 +164,33 @@ function generateQuizFeedback() {
   return html;
 }
 
-
 function generateQuizCount() {
   // current question, total questions, and total questions right
-  return `<h3>Question ${(store.questionNumber + 1)}/${store.questions.length}. ${store.score} correct.</h3>`;
+  return `<h3>Question ${store.questionNumber + 1}/${store.questions.length}. ${
+    store.score
+  } correct.</h3>`;
 }
 
 function generateTitleScreen() {
   //generate our title screen and restart
-  return `<section class = 'js-main-screen'>
-              <h2 class = 'center-text'>Test your Risk of Rain knowledge with this quiz!</h2> 
-              <button type='submit' class = 'js-start-button'>Start!</button>
+  return `<section class = "js-main-screen">
+              <h2 class = "center-text">Test your Risk of Rain knowledge with this quiz!</h2> 
+              <button type="submit" class = "js-start-button">Start!</button>
           </section>`;
 }
 
 function generateEndScreen() {
   // generate our quiz results
-  return `<section class='js-main-screen'>
-            <div class='results-container'>
-              <h2 class='js-results-text'>Your Results</h2>
-              <h3 class='js-results-text'>You got ${store.score} out of ${store.questions.length} right!</h2>
-              <h3 class='js-results-text'>${Math.floor((store.score / store.questions.length) * 100)}%</h2>
-              <button type='submit' class='js-end-button'>Retake the quiz!</button>    
+  return `<section class="js-main-screen">
+            <div class="results-container">
+              <h2 class="js-results-text">Your Results</h2>
+              <h3 class="js-results-text">You got ${store.score} out of ${
+    store.questions.length
+  } right!</h2>
+              <h3 class="js-results-text">${Math.floor(
+                (store.score / store.questions.length) * 100
+              )}%</h2>
+              <button type="submit" class="js-end-button">Retake the quiz!</button>    
             </div>
           </section>`;
 }
@@ -184,7 +199,6 @@ function generateEndScreen() {
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
 function renderQuizScreen() {
-
   let generateString = '';
   if (!store.quizStarted) {
     generateString = generateTitleScreen();
@@ -213,8 +227,7 @@ function handleAnswerSubmitted() {
 
 function handleNextQuestion() {
   $('main').on('click', '.js-continue-button', (event) => {
-    store.answer = '',
-      renderQuizScreen();
+    (store.answer = ''), renderQuizScreen();
   });
 }
 
@@ -223,7 +236,6 @@ function handleStartQuiz() {
     event.preventDefault();
     store.quizStarted = !store.quizStarted;
     renderQuizScreen();
-
   });
 }
 
